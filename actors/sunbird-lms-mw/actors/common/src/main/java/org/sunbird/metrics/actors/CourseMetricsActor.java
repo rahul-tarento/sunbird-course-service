f7d989fbe73060564cdd93c260c67b09697beb0a
Rahul K <rahul.kumar@tarento.com>
2019-11-25 15:04:46 +0530

    replacing reports signed url with public url

--- actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/metrics/actors/CourseMetricsActor.java
+++ actors/sunbird-lms-mw/actors/common/src/main/java/org/sunbird/metrics/actors/CourseMetricsActor.java
@@ -333,8 +333,8 @@ public class CourseMetricsActor extends BaseMetricsActor {
                 + courseAssessmentsreportPath,
             LoggerEnum.INFO.name());
         assessmentReportSignedUrl =
-            CloudStorageUtil.getSignedUrl(
-                CloudStorageType.AZURE, courseMetricsContainer, courseAssessmentsreportPath);
+            CloudStorageUtil.getUri(
+                CloudStorageType.AZURE, courseMetricsContainer, courseAssessmentsreportPath, false);
       }
     }
 
@@ -345,7 +345,7 @@ public class CourseMetricsActor extends BaseMetricsActor {
             + reportPath,
         LoggerEnum.INFO.name());
     String signedUrl =
-        CloudStorageUtil.getSignedUrl(CloudStorageType.AZURE, courseMetricsContainer, reportPath);
+        CloudStorageUtil.getUri(CloudStorageType.AZURE, courseMetricsContainer, reportPath, false);
 
     Response response = new Response();
     response.put(JsonKey.SIGNED_URL, signedUrl);
